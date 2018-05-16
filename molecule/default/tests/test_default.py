9404b9eb2c43cef1c1327b9a1b140d894cf9268b
coaxial <hi@64b.it>
2018-05-16 16:27:56 -0400

    Test compose file

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,6 +15,22 @@ def test_hosts_file(host):
 
 
 def test_firewall(host):
-    r = host.iptables.rules()
+    r = host.iptables.rules('filter', 'DOCKER-USER')
 
     assert "-A DOCKER-USER -i eth0 -p tcp -m tcp --dport 5000 -j DROP" in r
+
+
+def test_compose_extends(host):
+    dc = host.file('/opt/webapp/docker-compose.proxy.yml')
+
+    assert dc.exists
+    assert dc.user == 'root'
+    assert dc.group == 'root'
+    assert dc.contains('webapp:')
+    assert dc.contains('VIRTUAL_HOST=test.example.org')
+    assert dc.contains('VIRTUAL_PORT=5000')
+    assert dc.contains('LETSENCRYPT_HOST=test.example.org')
+    assert dc.contains('LETSENCRYPT_EMAIL=test@example.org')
+    assert not dc.contains('LETSENCRYPT_TEST=true')
+    assert dc.contains('upstreams:')
+    assert dc.contains('- upstreams')
