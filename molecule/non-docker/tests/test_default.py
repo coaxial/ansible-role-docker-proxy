4e8393bff7ebbaabb52bcd1922eeed994662497f
coaxial <hi@64b.it>
2018-06-21 11:19:10 -0400

    Restart nginx proxy before testing

--- molecule/non-docker/tests/test_default.py
+++ molecule/non-docker/tests/test_default.py
@@ -52,6 +52,7 @@ def test_proxy(host):
     #     ' && curl -vL http://localhost:1500'
     # )
     # host.run('curl -vL http://localhost:1500')
+    host.run('cd /opt/webapp && sudo docker-compose restart')
     webpage = host.check_output(
         '(printf "HTTP/1.1 200 OK\r\n'
         'Content-length: 13\r\n\r\nHello world!\r\n" | nc -q 1 -l -p 1500 &)'
