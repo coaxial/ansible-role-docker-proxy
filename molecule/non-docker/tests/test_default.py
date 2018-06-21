6d5bec3481527ec8491c580259c9b11d0fb9d2d9
coaxial <hi@64b.it>
2018-06-21 12:31:45 -0400

    debug

--- molecule/non-docker/tests/test_default.py
+++ molecule/non-docker/tests/test_default.py
@@ -52,6 +52,7 @@ def test_proxy(host):
     #     ' && curl -vL http://localhost:1500'
     # )
     # host.run('curl -vL http://localhost:1500')
+    host.run('sudo docker logs nginx-proxy')
     webpage = host.check_output(
         '(printf "HTTP/1.1 200 OK\r\n'
         'Content-length: 13\r\n\r\nHello world!\r\n" | nc -q 1 -l -p 1500 &)'
