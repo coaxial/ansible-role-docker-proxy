cc48df49630ee024ba4061ac60c854800b2466a9
coaxial <hi@64b.it>
2018-06-23 08:41:16 -0400

    debug

--- molecule/non-docker/tests/test_default.py
+++ molecule/non-docker/tests/test_default.py
@@ -53,41 +53,53 @@ def test_proxy(host):
     # )
     # host.run('curl -vL http://localhost:1500')
 
-    # make test.example.org resolve so that it can be curled and nginx-proxy
+    # Make test.example.org resolve so that it can be curled and nginx-proxy
     # knows which container to forward it to based on the headers
     host.run('127.0.0.1 test.example.org >> /etc/hosts')
 
-    host.run('sudo docker logs nginx-proxy')
-    host.run('sudo docker logs nginx-webapp')
-    host.run(
+    # host.run('sudo docker logs nginx-proxy')
+    # host.run('sudo docker logs nginx-webapp')
+    # host.run(
+    #     # This is a minimal webserver that will answer with 200 OK
+    #     # and Hello world!
+    #     'sh -c \'(while true; do printf "HTTP/1.1 200 OK\r\n'
+    #     'Content-length: 13\r\nContent-type: text/plain\r\n\r\n'
+    #     'Hello world!\r\n" | nc -q 1 -l -p 1500;'
+    #     ' done) &\''
+    #     # ' && docker restart nginx-proxy'
+    #     # ' && sudo docker restart nginx-webapp'' && curl -vL '
+    # 'http://localhost'
+    #     # ' && export GWIP=`{% raw -%}docker network inspect bridge --format'
+    #     # ' \'{{range .IPAM.Config}}{{.Gateway}}{{end}}\'{% endraw -%}`'
+    #     ' && sleep 2 && sudo docker exec nginx-proxy sh -c "apt update 2>&1'
+    #     '&& apt install inetutils-ping curl -yq 2>&1 && ping -c 5 172.18.0.1'
+    #     # ' && curl -vL 172.18.0.1:1500'
+    #     ' && curl -vL nginx-webapp"'
+    #     # ' && sleep 2 && curl -vL http://${GWIP}:1500'
+    #     ' && sleep 2 && curl -vL http://localhost:1500'
+    #     ' && sleep 2 && curl -vL http://localhost'
+    #     # ' && sudo docker inspect nginx-webapp'
+    #     # ' && sudo docker inspect nginx-proxy'
+    #     ' && echo "127.0.0.1 test.example.org" >> /etc/hosts'
+    #     ' && curl -vL test.example.org'
+    #     ' && echo "nginx-proxy logs:"'
+    #     ' && sudo docker logs nginx-proxy'
+    #     ' && echo "nginx-webapp logs:"'
+    #     ' && sudo docker logs nginx-webapp'
+    # )
+    # Query the minimal webserver through nginx-proxy
+    webpage = host.check_output(
+        # This is a minimal webserver that will answer with 200 OK
+        # and Hello world!
         'sh -c \'(while true; do printf "HTTP/1.1 200 OK\r\n'
         'Content-length: 13\r\nContent-type: text/plain\r\n\r\n'
         'Hello world!\r\n" | nc -q 1 -l -p 1500;'
         ' done) &\''
-        # ' && docker restart nginx-proxy'
-        # ' && sudo docker restart nginx-webapp'' && curl -vL http://localhost'
-        # ' && export GWIP=`{% raw -%}docker network inspect bridge --format '
-        # '\'{{range .IPAM.Config}}{{.Gateway}}{{end}}\'{% endraw -%}`'
-        ' && sleep 2 && sudo docker exec nginx-proxy sh -c "apt update 2>&1'
-        '&& apt install inetutils-ping curl -yq 2>&1 && ping -c 5 172.18.0.1'
-        # ' && curl -vL 172.18.0.1:1500'
-        ' && curl -vL nginx-webapp"'
-        # ' && sleep 2 && curl -vL http://${GWIP}:1500'
-        ' && sleep 2 && curl -vL http://localhost:1500'
-        ' && sleep 2 && curl -vL http://localhost'
-        # ' && sudo docker inspect nginx-webapp'
-        # ' && sudo docker inspect nginx-proxy'
-        ' && echo "127.0.0.1 test.example.org" >> /etc/hosts'
-        ' && curl -vL test.example.org'
-        ' && echo "nginx-proxy logs:"'
-        ' && sudo docker logs nginx-proxy'
-        ' && echo "nginx-webapp logs:"'
-        ' && sudo docker logs nginx-webapp'
+        ' && curl -sfL test.example.org'
     )
-    webpage = host.check_output('curl -sfL test.example.org')
     # webpage = host.check_output('curl -sfL http://localhost')
 
-    host.run('sudo docker logs nginx-gen')
+    # host.run('sudo docker logs nginx-gen')
     assert "Hello world!" in webpage
     # assert "gogoighrioeghiroeugoilhgoia" in webpage
 
