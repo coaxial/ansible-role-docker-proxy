d035405c33875f2950a0b4ba0c94e33fe5945363
coaxial <hi@64b.it>
2018-06-24 13:42:33 -0400

    debug

--- molecule/non-docker-custom/tests/test_default.py
+++ molecule/non-docker-custom/tests/test_default.py
@@ -58,9 +58,10 @@ def test_override(host):
 
 def test_proxy(host):
     hello = host.check_output(
-        'sh -c \'' + webserver + '\''
-        # Query the minimal webserver through nginx-proxy
-        ' && curl -sfL http://test.example.org/hello/'
+        # 'sh -c \'' + webserver + '\''
+        # # Query the minimal webserver through nginx-proxy
+        # ' && curl -sfL http://test.example.org/hello/'
+        'curl -sfL http://test.example.org/hello/'
     )
 
     assert "Hello world!" in hello
